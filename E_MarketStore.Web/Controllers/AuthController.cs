7d84bbed27add3311a092fb7a3b49c0ed6a59915
gintex232 <amahuo.godwin232@gmail.com>
2023-11-08 14:24:32 +0100

    login in action

--- E_MarketStore.Web/Controllers/AuthController.cs
+++ E_MarketStore.Web/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using E_MarketStore.Service.IService;
 using Microsoft.AspNetCore.Mvc;
 using E_MarketStore.Web.Utility;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace E_MarketStore.Web.Controllers
 {
@@ -22,6 +23,34 @@ namespace E_MarketStore.Web.Controllers
             return View(loginRequestDto);
         }
 
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequestDto obj)
+        {
+            ResponseDto responseDto = await _authService.LoginAsync(obj);
+
+            if (responseDto != null && responseDto.IsSuccess)
+            {
+                LoginResponseDto loginResponseDto = JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(responseDto.Result));
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("CustomError", responseDto.Message);
+                return View(obj);
+            }
+
+
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{Text=Enums.RoleAdmin, Value=Enums.RoleAdmin},
+                new SelectListItem{Text=Enums.RoleCustomer, Value=Enums.RoleCustomer}
+            };
+            ViewBag.RoleList = roleList;
+            return View(obj);
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -32,6 +61,40 @@ namespace E_MarketStore.Web.Controllers
             };
             ViewBag.RoleList = roleList;
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterRequestDto obj)
+        {
+            ResponseDto result = await _authService.RegisterAsync(obj);
+            ResponseDto assignRole;
+
+            if(result!=null && result.IsSuccess)
+            {
+                if (string.IsNullOrEmpty(obj.Role))
+                {
+                    obj.Role = Enums.RoleCustomer;
+                }
+                assignRole = await _authService.AssignRoleAsync(obj);
+                if (assignRole!=null && assignRole.IsSuccess)
+                {
+                    TempData["success"] = "Registration Successful";
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+            else
+            {
+                TempData["error"] = result.Message;
+            }
+
+
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{Text=Enums.RoleAdmin, Value=Enums.RoleAdmin},
+                new SelectListItem{Text=Enums.RoleCustomer, Value=Enums.RoleCustomer}
+            };
+            ViewBag.RoleList = roleList;
+            return View(obj);
         }
 
         public IActionResult Logout()
