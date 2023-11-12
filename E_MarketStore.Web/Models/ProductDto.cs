2047d3299f39162c797d90a7e8d6949373cb8f18
gintex232 <amahuo.godwin232@gmail.com>
2023-11-12 15:46:42 +0100

    home UI created

--- E_MarketStore.Web/Models/ProductDto.cs
+++ E_MarketStore.Web/Models/ProductDto.cs
@@ -1,4 +1,6 @@
-﻿namespace E_MarketStore.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_MarketStore.Web.Models
 {
     public class ProductDto
     {
@@ -8,5 +10,7 @@
         public string Description { get; set; }
         public string CategoryName { get; set; }
         public string ImageUrl { get; set; }
+        [Range(1, 100)]
+        public int Count { get; set; } = 1;
     }
 }
--- E_MarketStoreProductAPI/Controllers/ProductAPIController.cs
+++ E_MarketStoreProductAPI/Controllers/ProductAPIController.cs
@@ -10,7 +10,6 @@ namespace E_MarketStoreProductAPI.Controllers
 {
     [Route("api/product")]
     [ApiController]
-    [Authorize]
     public class ProductAPIController : ControllerBase
     {
         private readonly AppDbContext _db;
