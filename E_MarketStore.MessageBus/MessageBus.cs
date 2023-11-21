a5b3de2cc738d5b1679e2be5dbcfc6dcf0fd08aa
gintex232 <amahuo.godwin232@gmail.com>
2023-11-21 23:41:00 +0100

    adding message bus from azure

---
+++ E_MarketStore.MessageBus/MessageBus.cs
@@ -0,0 +1,19 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_MarketStore.MessageBus
+{
+    public class MessageBus : IMessageBus
+    {
+        private string connectionString = 
+
+        public async Task PublishMessage(object message, string topic_queue_Name)
+        {
+            await using var client = new ServiceBusClient()
+        }
+    }
+}
