eaf4561fa9a4a4928e372bde33001988d4adf79f
gintex232 <amahuo.godwin232@gmail.com>
2023-11-22 23:51:11 +0100

    messag bus

--- E_MarketStore.MessageBus/MessageBus.cs
+++ E_MarketStore.MessageBus/MessageBus.cs
@@ -1,4 +1,5 @@
 ﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,18 @@ namespace E_MarketStore.MessageBus
 
         public async Task PublishMessage(object message, string topic_queue_Name)
         {
-            await using var client = new ServiceBusClient()
+            await using var client = new ServiceBusClient(connectionString);
+
+            ServiceBusSender sender = client.CreateSender(topic_queue_Name);
+
+            var jsonMessage = JsonConvert.SerializeObject(message);
+            ServiceBusMessage finalMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(jsonMessage)) 
+            { 
+                CorrelationId = Guid.NewGuid().ToString(),
+            };
+
+            await sender.SendMessageAsync(finalMessage);
+            await client.DisposeAsync();
         }
     }
 }
