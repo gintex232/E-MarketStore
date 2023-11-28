5de681a80f707ff33ac2e1606ce2e397a734d429
gintex232 <amahuo.godwin232@gmail.com>
2023-11-28 23:36:49 +0100

    .

---
+++ E_MarketStore.Services.EmailAPI/Message/RewardsMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace E_MarketStore.Services.EmailAPI.Message
+{
+    public class RewardsMessage
+    {
+        public string UserId { get; set; }
+        public int RewardsActivity {  get; set; }
+        public int OrderId { get; set; }
+    }
+}
