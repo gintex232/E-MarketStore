a58fc73c5d8368d474e82f58f56fdbe8add51c47
gintex232 <amahuo.godwin232@gmail.com>
2023-12-02 00:55:36 +0100

    .

--- E_MarketStore.Services.EmailAPI/Models/Dto/CartHeaderDto.cs
+++ E_MarketStore.Services.EmailAPI/Models/Dto/CartHeaderDto.cs
@@ -1,14 +1,24 @@
 ﻿namespace E_MarketStore.Services.EmailAPI.Models.Dto
 {
-    public class CartHeaderDto
-    {
-        public int CartHeaderId { get; set; }
-        public string? UserId { get; set; }
-        public string? CouponCode { get; set; }
-        public double Discount { get; set; }
-        public double CartTotal { get; set; }
-        public string? Name { get; set; }
-        public string? Phone { get; set; }
-        public string? Email { get; set; }
-    }
+    //public class CartHeaderDto
+    //{
+    //    public int CartHeaderId { get; set; }
+    //    public string? UserId { get; set; }
+    //    public string? CouponCode { get; set; }
+    //    public double Discount { get; set; }
+    //    public double CartTotal { get; set; }
+    //    public string? Name { get; set; }
+    //    public string? Phone { get; set; }
+    //    public string? Email { get; set; }
+    //}
+
+    public record CartHeaderDto(
+        int CartHeaderId,
+        string? UserId,
+        string? CouponCode,
+        double Discount,
+        double CartTotal,
+        string? Name,
+        string? Phone,
+        string? Email);
 }
--- E_MarketStore.Services.EmailAPI/Models/Dto/ProductDto.cs
+++ E_MarketStore.Services.EmailAPI/Models/Dto/ProductDto.cs
@@ -1,8 +1,17 @@
 ﻿namespace E_MarketStore.Services.EmailAPI.Models.Dto
 {
-    public class ProductDto
-    {
-        public int ProductId { get; set; }
+    //public class ProductDto
+    //{
+    //    public int ProductId { get; set; }
 
-    }
+    //}
+
+    public record ProductDto(
+        int ProductId,
+        string Name,
+        double Price,
+        string Description,
+        string CategoryName,
+        string ImageUrl,
+        int Count);
 }
