f8d3f068a40e8b4ba74ba34a9438b70d388b0775
gintex232 <amahuo.godwin232@gmail.com>
2023-12-01 01:13:07 +0100

    .

--- E_MarketStore.Services.EmailAPI/Models/Dto/CartDetailsDto.cs
+++ E_MarketStore.Services.EmailAPI/Models/Dto/CartDetailsDto.cs
@@ -1,12 +1,20 @@
 ﻿namespace E_MarketStore.Services.EmailAPI.Models.Dto
 {
-    public class CartDetailsDto
-    {
-        public int CartDetaisId { get; set; }
-        public int CartHeaderId { get; set; }
-        public CartHeaderDto? CartHeader { get; set; }
-        public int ProductId { get; set; }
-        public ProductDto? Product { get; set; }
-        public int Count { get; set; }
-    } 
+    //public class CartDetailsDto
+    //{
+    //    public int CartDetaisId { get; set; }
+    //    public int CartHeaderId { get; set; }
+    //    public CartHeaderDto? CartHeader { get; set; }
+    //    public int ProductId { get; set; }
+    //    public ProductDto? Product { get; set; }
+    //    public int Count { get; set; }
+    //} 
+
+    public record CartDetailsDto(
+        int CartDetailsId,
+        int CartHeaderId,
+        CartHeaderDto? CartHeader,
+        int ProductId,
+        ProductDto? Product,
+        int Count);
 }
