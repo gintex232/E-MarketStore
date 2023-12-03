57b5443f5718423f6711508fe50517769718690d
gintex232 <amahuo.godwin232@gmail.com>
2023-12-03 14:01:35 +0100

    .

--- E_MarketStoreProductAPI/Models/DTO/ProductDto.cs
+++ E_MarketStoreProductAPI/Models/DTO/ProductDto.cs
@@ -2,13 +2,21 @@
 
 namespace E_MarketStoreProductAPI.Models.DTO
 {
-    public class ProductDto
-    {
-        public int ProductId { get; set; }
-        public string Name { get; set; }
-        public double Price { get; set; }
-        public string Description { get; set; }
-        public string CategoryName { get; set; }
-        public string ImageUrl { get; set; }
-    }
+    //public class ProductDto
+    //{
+    //    public int ProductId { get; set; }
+    //    public string Name { get; set; }
+    //    public double Price { get; set; }
+    //    public string Description { get; set; }
+    //    public string CategoryName { get; set; }
+    //    public string ImageUrl { get; set; }
+    //}
+
+    public record ProductDto(
+        int ProductId,
+        string Name,
+        double Price,
+        string Description,
+        string CategoryName,
+        string ImageUrl);
 }
