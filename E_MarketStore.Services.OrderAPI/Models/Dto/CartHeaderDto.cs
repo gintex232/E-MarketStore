351356e121b7c3fad910937952eb41f779137c67
gintex232 <amahuo.godwin232@gmail.com>
2024-04-16 19:14:04 +0100

    .

--- E_MarketStore.Services.OrderAPI/Models/Dto/CartHeaderDto.cs
+++ E_MarketStore.Services.OrderAPI/Models/Dto/CartHeaderDto.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿
 
 namespace E_MarketStore.Services.OrderAPI.Models.Dto
 {
--- E_MarketStore.Services.OrderAPI/obj/Debug/net8.0/E_MarketStore.Services.OrderAPI.AssemblyInfo.cs
+++ E_MarketStore.Services.OrderAPI/obj/Debug/net8.0/E_MarketStore.Services.OrderAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E_MarketStore.Services.OrderAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ad8fc10f6669371c556d3c560a1a112874bb335")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+419b4cac0de758b181bb99d6f5b278e6b5d4b718")]
 [assembly: System.Reflection.AssemblyProductAttribute("E_MarketStore.Services.OrderAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E_MarketStore.Services.OrderAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
